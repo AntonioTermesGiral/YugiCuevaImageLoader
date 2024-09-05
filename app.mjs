fa581b2e708d60c8f8cceebcb761d53c0ab75ec4
AntonioTermesGiral <antoniotermesgiral@gmail.com>
2024-09-05 12:36:54 +0200

    first commit, functionality working

---
+++ app.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import { config } from "dotenv";
+import { executeCheck } from "./src/main.mjs";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const handleRequest = (req, res) => {
+  console.log("requested!");
+  executeCheck().then((result) => res.type('json').send(result));
+}
+
+app.get("/", handleRequest);
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
---
+++ src/main.mjs
@@ -0,0 +1,58 @@
+// dependencies
+import request from 'request';
+import fs from 'fs';
+import { createClient } from '@supabase/supabase-js';
+import { config } from "dotenv";
+
+config();
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_PUBLIC_KEY);
+
+const doLogin = async () => {
+
+    const { data: user, error: userErr } = await supabase.auth.signInWithPassword({
+        email: process.env.SUPABASE_USER,
+        password: process.env.SUPABASE_PASS,
+    });
+
+    userErr && console.log("Login error: ", userErr);
+    console.log(user.session.access_token);
+}
+
+const getCardIds = async (getAll) => {
+    const { data, error } = await supabase.from('card').select("id, image");
+    error && console.log("Card fetch error: ", error);
+
+    const noImageCards = data.filter(card => card.image == null).map(card => card.id);
+    return getAll ? data : noImageCards;
+}
+
+const uploadImage = async (id) => {
+    // Gets the image data
+    const cardURL = process.env.YGOPD_IMAGE_BASE + id + ".jpg";
+    const response = await fetch(cardURL);
+    const blob = await response.blob();
+    const file = new File([blob], `${id}.jpg`, { type: blob.type });
+    console.log("File being uploaded: ", file);
+
+    // Uploads the file to the bucket
+    const { data: uploadData, error: uploadError } = await supabase.storage.from('CardImages')
+        .upload(`${id}.jpg`, file, { cacheControl: '3600', upsert: false });
+
+    console.log("File uploaded: ", uploadData);
+    console.log("File uploaded id: ", uploadData.id);
+    uploadError && console.log("File upload error: ", uploadError);
+
+    // Updates the card with the image id
+    const { data: updateData, error: updateError } = await supabase.from('card')
+        .update({ image: uploadData.id }).eq('id', id).select();
+
+    console.log("Card updated: ", updateData);
+    updateError && console.log("Card update error: ", updateError);
+}
+
+export const executeCheck = async () => {
+    await doLogin();
+    const cardIds = await getCardIds();
+    cardIds.forEach(uploadImage);
+    return Promise.resolve({ success: true });
+}
